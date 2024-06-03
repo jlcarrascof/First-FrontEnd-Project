74379682867e93ac9c596ad403630f61b9896c5a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-02 20:33:59 -0400

    style: Update hamburger menu icon functionality and improve navigation bar styles

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,16 @@
 const hamburger = document.querySelector(".nav__icon");
 const menu = document.querySelector(".nav__list");
+let open = true;
 
 hamburger.addEventListener("click", () => {
+
+    if (open) {
+        hamburger.src = "./images/icon-close.svg";
+        open = false;
+    } else {
+        hamburger.src = "./images/icon-hamburger.svg";
+        open = true;
+    }
+
     menu.classList.toggle("nav__list--show");
 });
